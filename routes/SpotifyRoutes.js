07d014eb81ca0dc26e8eb89da864519b3054f4d2
lentusignavus <ignavus.ii@gmail.com>
2015-09-21 11:12:20 -0700

    Major strides in Spotify authentication

--- routes/SpotifyRoutes.js
+++ routes/SpotifyRoutes.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 
 var base_auth_uri = function (clientId) {
-  return uri = process.env.SPOTIFY_AUTH_BASE_URI + "authorize/?client_id=" + clientId +"&response_type=code&redirect_uri=http://localhost:3000";
+  return uri = process.env.SPOTIFY_AUTH_BASE_URI + "authorize/?client_id=" + clientId +"&response_type=code&redirect_uri=" + process.env.REDIRECT_URI;
 
 }
 
